6c896e78778f9dc8cf6ab8c570a6afaef4cd1b21
Anusha <anusha6113@gmail.com>
2026-04-29 10:13:08 +0530

    Initial Django Project

---
+++ student_project/student_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    roll_number = models.CharField(null=True, blank=True)
+    course = models.CharField(max_length=100)
+    extra_data = models.JSONField(null=True, blank=True)
+       
+
+
+    
+
+    def __str__(self):
+        return self.name
+    
---
+++ student_project/student_app/views.py
@@ -0,0 +1,46 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+
+
+# GET all + POST
+@api_view(['GET', 'POST'])
+def student_list(request):
+
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+# GET one + PUT + DELETE
+@api_view(['GET', 'PUT', 'DELETE'])
+def student_detail(request, pk):
+
+    try:
+        student = Student.objects.get(id=pk)
+    except Student.DoesNotExist:
+        return Response({"error": "Student not found"})
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response({"message": "Deleted successfully"})
