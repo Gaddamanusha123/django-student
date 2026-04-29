d9a677c7b412a25788c85dd64f0ba688e58865cf
Anusha <anusha6113@gmail.com>
2026-04-29 12:30:19 +0530

    changes in student_project

--- student_project/student_app/admin.py
+++ student_project/student_app/admin.py
@@ -1,7 +1,43 @@
 from django.contrib import admin
 from .models import Student
 
+@admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age', 'course')
-    
-admin.site.register(Student, StudentAdmin)
+
+    # List page columns
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return ('id', 'name', 'age', 'course', 'roll_number')
+        return ('id', 'name', 'course')
+
+    # Form fields
+    def get_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return ('name', 'age', 'course', 'roll_number')
+        return ('name', 'course')   # ✅ keep name visible
+
+    # Readonly fields
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return ()
+        return ()   # ✅ allow editing name (remove readonly)
+
+    # Delete permission
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    # Add permission
+    def has_add_permission(self, request):
+        return True
+
+    # Change permission
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    # ✅ IMPORTANT FIX (auto handle missing fields)
+    def save_model(self, request, obj, form, change):
+        if not obj.age:
+            obj.age = 0   # default value
+        if not obj.roll_number:
+            obj.roll_number = "N/A"
+        super().save_model(request, obj, form, change)
--- student_project/student_app/models.py
+++ student_project/student_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    age = models.IntegerField(null=True, blank=True)   
     roll_number = models.CharField(null=True, blank=True)
     course = models.CharField(max_length=100)
     extra_data = models.JSONField(null=True, blank=True)
@@ -14,3 +14,6 @@ class Student(models.Model):
     def __str__(self):
         return self.name
     
+
+
+
